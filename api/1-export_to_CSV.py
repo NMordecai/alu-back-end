6585780c54b14323ec8f23a3808515a3b1f31098
Momo <m.nayiturik@alustudent.com>
2025-06-26 08:02:39 +0000

    Python script to export data in the CSV format.

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
+"""
+Exports tasks for a given employee ID to a CSV file.
+"""
 
 import csv
 import requests
 import sys
 
-
-def main():
-    """main function"""
-    user_id = int(sys.argv[1])
-    todo_url = 'https://jsonplaceholder.typicode.com/todos'
-    user_url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-
-    file_content = []
-
-    response = requests.get(todo_url)
-    user_name = requests.get(user_url).json().get('username')
-
-    for todo in response.json():
-        if todo.get('userId') == user_id:
-            file_content.append(
-                [str(user_id),
-                 user_name,
-                 todo.get('completed'),
-                 "{}".format(todo.get('title'))])
-
-    print(file_content)
-    file_name = "{}.csv".format(user_id)
-    with open(file_name, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        for row in file_content:
-            for item in row:
-                str(item)
-            csv_writer.writerow(row)
-        print('file written successfully')
-
-
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 2 or not sys.argv[1].isdigit():
+        print("Usage: ./1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_url = (
+        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+    )
+
+    user_response = requests.get(user_url)
+    if user_response.status_code != 200:
+        print("User not found")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    username = user_data.get("username")
+
+    todos_response = requests.get(todos_url)
+    todos = todos_response.json()
+
+    filename = f"{employee_id}.csv"
+    with open(filename, mode="w", newline="", encoding="utf-8") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for task in todos:
+            writer.writerow([
+                employee_id,
+                username,
+                task.get("completed"),
+                task.get("title")
+            ])
